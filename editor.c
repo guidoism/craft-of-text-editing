4c7b628f0ad198a6971034895aa43e1e591c2b98
Guido Bartolucci <guido@bartolucci.org>
2023-02-15 23:15:28 -0500

    read in raw input

--- editor.c
+++ editor.c
@@ -2,6 +2,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 typedef struct Buffer {
   char     name[64];
@@ -24,13 +27,37 @@ uint16_t create_buffer(const char* name) {
   return last_buffer;
 }
 
+struct termios orig_termios;
+void disable_raw_mode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enable_raw_mode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main(void) {
-  int16_t b = create_buffer("*scratch*");
-  printf("Buffer: %s\n", buffers[b].name);
-  int16_t b2 = create_buffer("*scratch*");
-  printf("Buffer: %d\n", b2);
+  enable_raw_mode();
+  create_buffer("*scratch*");
+  
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
 
 
-// Test: Make two buffers with the same name and get error
+
+  // Test: Make two buffers with the same name and get error
+  /*
+  int16_t b = create_buffer("*scratch*");
+  printf("Buffer: %s\n", buffers[b].name);
+  int16_t b2 = create_buffer("*scratch*");
+  printf("Buffer: %d\n", b2);
+  */
+  
+
+
