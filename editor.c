d25aaf791064797fec2cd825a73262bca088646a
Guido Bartolucci <guido@bartolucci.org>
2023-02-15 22:39:05 -0500

    Checkpoint

--- editor.c
+++ editor.c
@@ -5,9 +5,9 @@
 
 typedef struct Buffer {
   char     name[64];
-  uint32_t point;
-  uint16_t id;
-  uint16_t next;
+  int32_t  point;
+  int16_t  id;
+  int16_t  next;
 } Buffer;
 
 Buffer buffers[64]; // TODO: make expandable
@@ -15,14 +15,22 @@ uint16_t last_buffer = 0;
 
 // Returns buffer id
 uint16_t create_buffer(const char* name) {
+  for (int i = 0; i <= last_buffer; i++) {
+    if (strcmp(name, buffers[i].name) == 0) {
+      return -1;
+    }
+  }
   strncpy(buffers[++last_buffer].name, name, 64);
   return last_buffer;
 }
 
 int main(void) {
-  uint16_t b = create_buffer("*scratch*");
+  int16_t b = create_buffer("*scratch*");
   printf("Buffer: %s\n", buffers[b].name);
-  uint16_t b2 = create_buffer("*scratch*");
-  printf("Buffer: %s\n", buffers[b2].name);
+  int16_t b2 = create_buffer("*scratch*");
+  printf("Buffer: %d\n", b2);
   return 0;
 }
+
+
+// Test: Make two buffers with the same name and get error
