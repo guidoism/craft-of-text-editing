a9b7a8c6d222824381f055c5e043b0b4893d869e
Guido Bartolucci <guido@bartolucci.org>
2023-02-15 20:53:15 -0500

    Checkpoint

--- editor.c
+++ editor.c
@@ -1,5 +1,15 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+typedef struct Buffer {
+  char name[64];
+  int point;
+  struct Buffer* next;
+} Buffer;
 
 int main(void) {
+  Buffer b = {.name="*scratch*"};
+  printf("Buffer: %s\n", b.name);
   return 0;
 }
